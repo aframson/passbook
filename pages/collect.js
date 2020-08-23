93d312053f608e6c4e156206db954a1edabcaffe
Richard Obiri <obiriaframson@gmail.com>
2020-08-23 04:12:40 -0700

    x

--- pages/collect.js
+++ pages/collect.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+ import React, { Component } from 'react';
 import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity, ActivityIndicator,FlatList } from 'react-native';
 
 
